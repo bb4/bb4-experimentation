056b746fd359e0c1d0177884d1ac9cf092c27968
barrybecker4 <barrybecker4@yahoo.com>
2018-10-07 04:23:28 -0700

    Split client ui and socket code.

--- scala-source/com/barrybecker4/experimentation/socket/Client.scala
+++ scala-source/com/barrybecker4/experimentation/socket/Client.scala
@@ -13,82 +13,44 @@ import java.net.UnknownHostException
 
 
 /**
-  * Socket portion of client-server program using sockets.
-  * Adapted from http://java.sun.com/developer/onlineTraining/Programming/BasicJava2/socket.html
+  * Socket connection to server
   * @author Barry Becker
   */
-object Client extends App {
-  private val DEFAULT_HOST = "127.0.0.1" /// "192.168.1.100";
-
+object Client {
   private def exceptionOccurred(msg: String, t: Throwable): Unit = throw new IllegalStateException(msg, t)
-
-  val client = new Client
-  client.setTitle("Client Program")
-  client.addWindowListener(new WindowAdapter() {
-    override def windowClosing(e: WindowEvent): Unit = {System.exit(0)}
-  })
-  client.pack()
-  client.setVisible(true)
-  client.createListenSocket()
 }
 
-class Client() extends JFrame with ActionListener {
-  initUI()
+class Client(host: String, port: Int) {
+
   private var out: PrintWriter = _
   private var in: BufferedReader = _
-  private var button: JButton = _
-  private var textField: JTextField = _
 
-  private def initUI(): Unit = {
-    val text = new JLabel("Text to send over socket:")
-    textField = new JTextField(40)
-    button = new JButton("Send message")
-    button.setToolTipText("Send data to server")
-    button.addActionListener(this)
-    val buttonPanel = new JPanel(new FlowLayout)
-    buttonPanel.add(new JPanel)
-    buttonPanel.add(button)
-    buttonPanel.add(new JPanel)
-    val panel: JPanel = new JPanel
-    panel.setLayout(new BorderLayout)
-    panel.setBackground(Color.white)
-    getContentPane.add(panel)
-    panel.add("North", text)
-    panel.add("Center", textField)
-    panel.add("South", buttonPanel)
+  try {
+    val socket: Socket = new Socket(host, port)
+    out = new PrintWriter(socket.getOutputStream, true)
+    in = new BufferedReader(new InputStreamReader(socket.getInputStream))
+    println("create listen out_1 = " + out)
+  } catch {
+    case e: UnknownHostException =>
+      Client.exceptionOccurred("Unknown host: " + host, e)
+    case e: IOException =>
+      Client.exceptionOccurred("No I/O", e)
   }
+  assert(out != null, "Failed to create socket")
 
-  /** @param event button click to transmit text.*/
-  override def actionPerformed(event: ActionEvent): Unit = {
-    val source = event.getSource
-    if (source eq button) { //Send data over socket
-      val text = textField.getText
-      out.println(text)
-      textField.setText("")
-      //Receive text from server
-      try {
-        val line = in.readLine
-        System.out.println("Text received:" + line)
-      } catch {
-        case e: IOException =>
-          Client.exceptionOccurred("Read failed", e)
-      }
-    }
-  }
+  /** @param text text to send to server */
+  def send(text: String): Unit = out.println(text)
 
-  def createListenSocket(): Unit = {
+  /** @return text received from server */
+  def receive(): String = {
+    var line: String = null
     try {
-      println("client host = " + Client.DEFAULT_HOST + ", port = "+ Server.PORT)
-      val socket: Socket = new Socket(Client.DEFAULT_HOST, Server.PORT)
-      out = new PrintWriter(socket.getOutputStream, true)
-      in = new BufferedReader(new InputStreamReader(socket.getInputStream))
-      println("create listen out_1 = " + out)
+      val line = in.readLine
+      return line
     } catch {
-      case e: UnknownHostException =>
-        Client.exceptionOccurred("Unknown host: " + Client.DEFAULT_HOST, e)
       case e: IOException =>
-        Client.exceptionOccurred("No I/O", e)
+        Client.exceptionOccurred("Read failed", e)
     }
-    assert(out != null, "Failed to create socket")
+    line
   }
 }
---
+++ scala-source/com/barrybecker4/experimentation/socket/ClientFrame.scala
@@ -0,0 +1,64 @@
+/** Copyright by Barry G. Becker, 2000-2018. Licensed under MIT License: http://www.opensource.org/licenses/MIT  */
+package com.barrybecker4.experimentation.socket
+
+import java.awt._
+import java.awt.event._
+import java.io.{BufferedReader, IOException, InputStreamReader, PrintWriter}
+import java.net.{Socket, UnknownHostException}
+import javax.swing._
+
+
+/**
+  * Socket portion of client-server program using sockets.
+  * Adapted from http://java.sun.com/developer/onlineTraining/Programming/BasicJava2/socket.html
+  * @author Barry Becker
+  */
+object ClientFrame extends App {
+  private val DEFAULT_HOST = "127.0.0.1" /// "192.168.1.100";
+  new ClientFrame("Client Program", DEFAULT_HOST, Server.PORT)
+}
+
+class ClientFrame(title: String, host: String, port: Int) extends JFrame with ActionListener {
+  initUI()
+
+  setTitle(title)
+  addWindowListener(new WindowAdapter() {
+    override def windowClosing(e: WindowEvent): Unit = {System.exit(0)}
+  })
+  pack()
+  setVisible(true)
+  val client = new Client(host, port)
+
+  private var button: JButton = _
+  private var textField: JTextField = _
+
+  private def initUI(): Unit = {
+    val text = new JLabel("Text to send over socket:")
+    textField = new JTextField(40)
+    button = new JButton("Send message")
+    button.setToolTipText("Send data to server")
+    button.addActionListener(this)
+    val buttonPanel = new JPanel(new FlowLayout)
+    buttonPanel.add(new JPanel)
+    buttonPanel.add(button)
+    buttonPanel.add(new JPanel)
+    val panel: JPanel = new JPanel
+    panel.setLayout(new BorderLayout)
+    panel.setBackground(Color.white)
+    getContentPane.add(panel)
+    panel.add("North", text)
+    panel.add("Center", textField)
+    panel.add("South", buttonPanel)
+  }
+
+  /** @param event button click to transmit text.*/
+  override def actionPerformed(event: ActionEvent): Unit = {
+    val source = event.getSource
+    if (source eq button) { //Send data over socket
+      client.send(textField.getText)
+      textField.setText("")
+
+      println("Text received:" + client.receive())
+    }
+  }
+}
